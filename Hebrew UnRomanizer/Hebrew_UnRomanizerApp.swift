aa0c2905de12370d5f1951cdd280543fd1e9f0bb
Richard Todd <rwtodd@users.noreply.github.com>
2023-09-15 23:45:09 -0500

    Initial Commit

---
+++ Hebrew UnRomanizer/Hebrew_UnRomanizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hebrew_UnRomanizerApp.swift
+//  Hebrew UnRomanizer
+//
+//  Created by Richard Todd on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Hebrew_UnRomanizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
